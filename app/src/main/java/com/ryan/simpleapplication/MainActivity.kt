1e413ac4217e0efb5f1723807744b067ce6c41b6
Ryan Hasbie <105176413+ryanhasbie@users.noreply.github.com>
2023-05-01 12:44:31 +0700

    make button to onclick listener

--- app/src/main/java/com/ryan/simpleapplication/MainActivity.kt
+++ app/src/main/java/com/ryan/simpleapplication/MainActivity.kt
@@ -2,6 +2,7 @@ package com.ryan.simpleapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.Toast
 
 
@@ -9,14 +10,18 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        // Get id from text view
-        // val myTextView = findViewById<TextView>(R.id.text_view)
-        // Update text in id = text_view
-        // myTextView.text = "New Text View"
-        showToast("Hello Ryan..")
+        buttonCalculateOnClickListener()
     }
 
     private fun showToast(message:String) {
         Toast.makeText(applicationContext, message, Toast.LENGTH_SHORT).show()
     }
+
+    private fun buttonCalculateOnClickListener() {
+        val buttonCalculate = findViewById<Button>(R.id.button_calculate)
+        buttonCalculate.setOnClickListener {
+            showToast("Hello Ryan!")
+        }
+    }
+
 }
