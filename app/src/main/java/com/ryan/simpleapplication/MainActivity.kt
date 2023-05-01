e1209392b5d9d53b54660f0d74420ac267d9865d
Ryan Hasbie <105176413+ryanhasbie@users.noreply.github.com>
2023-05-01 14:22:42 +0700

    Choose operation in radio button

--- app/src/main/java/com/ryan/simpleapplication/MainActivity.kt
+++ app/src/main/java/com/ryan/simpleapplication/MainActivity.kt
@@ -4,11 +4,14 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import android.widget.RadioButton
+import android.widget.RadioGroup
 import android.widget.TextView
 import android.widget.Toast
 
 
 class MainActivity : AppCompatActivity() {
+    private final var OPERATION:String? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -16,7 +19,13 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun calculator (number1:Int, number2:Int) : String {
-        val result = number1 + number2
+        var result: Int = 0
+        when (OPERATION) {
+            "+" -> result = number1 + number2
+            "-" -> result = number1 - number2
+            "x" -> result = number1 * number2
+            ":" -> result = number1 / number2
+        }
         return result.toString()
     }
 
@@ -29,14 +38,23 @@ class MainActivity : AppCompatActivity() {
         buttonCalculate.setOnClickListener {
             val editTextNumber1 = findViewById<EditText>(R.id.edit_number1)
             val editTextNumber2 = findViewById<EditText>(R.id.edit_number2)
+            val radioGroupOperations = findViewById<RadioGroup>(R.id.radio_group_operation)
+            val textViewResult = findViewById<TextView>(R.id.text_result)
 
             if (editTextNumber1.text.isNullOrEmpty() || editTextNumber2.text.isNullOrEmpty()) {
                 showToast("Input is invalid...")
+            } else if (OPERATION.isNullOrEmpty()) {
+                showToast("Choose operation...")
             } else {
                 val editTextNumber1Value = editTextNumber1.text.toString().toInt()
                 val editTextNumber2Value = editTextNumber2.text.toString().toInt()
-                val textViewResult = findViewById<TextView>(R.id.text_result)
                 textViewResult.text = calculator(editTextNumber1Value, editTextNumber2Value)
+            }
+
+            radioGroupOperations.setOnCheckedChangeListener { group, checkedId ->
+                val radioButton = findViewById<RadioButton>(group.checkedRadioButtonId)
+                OPERATION = radioButton.text.toString()
+                textViewResult.text = "Result"
             }
         }
     }
