76ec41f2f4c35feac37dc168ae59be7bb884596c
Ryan Hasbie <105176413+ryanhasbie@users.noreply.github.com>
2023-05-01 11:13:01 +0700

    layouting with xml:

--- app/src/main/java/com/ryan/simpleapplication/MainActivity.kt
+++ app/src/main/java/com/ryan/simpleapplication/MainActivity.kt
@@ -3,9 +3,15 @@ package com.ryan.simpleapplication
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+
+
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        // Get id from text view
+        // val myTextView = findViewById<TextView>(R.id.text_view)
+        // Update text in id = text_view
+        // myTextView.text = "New Text View"
     }
 }
